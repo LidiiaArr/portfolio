bee5d26461b259eb99eb905fe8ae7182bf882a99
Lidiia A <lida1450@yandex.ru>
2023-04-24 15:53:33 +0300

    media

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
     return (
         <div className={styles.projectsBlock}>
             <div className={`${styleContainer.container} ${styles.projectsContainer}`}>
-                <h2 className={styles.title}>Мои проэкты</h2>
+                <h2 className={styles.title}>Мои проекты</h2>
                 <div className={styles.projects}>
                     <Project title={"Социальная сеть"} description={"TypeScript React Yarn классная социальная сеть"}/>
                     <Project title={"ToDoList"} description={"TypeScript React Yarn Jest классный тудулист"}/>
